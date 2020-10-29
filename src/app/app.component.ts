c6148342f23b0c9291048ecfd3491afd5f961521
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-10-29 21:29:33 +0100

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CallApiService } from './shared/services/call-api.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'currency-converter';
+  texto: string = '';
+  moneda: string = 'USD';
+
+  show: boolean = false;
+  constructor(private callApi: CallApiService) { }
+
+
+  converter = new Promise((resolve, reject) => {
+    let url: string = 'https://api.openrates.io/latest';
+    this.callApi.getData(url).subscribe((data: any) => {
+
+      setInterval(() => {
+        let coin_1 = Number(this.texto);
+        let coin_2 = Number(data.rates[this.moneda.toUpperCase()]);
+        let total = coin_1 * coin_2;
+        if (this.show) {
+          total = Math.round(total*100);
+          resolve(total/100);
+          
+        }
+
+      }, 1000);
+
+
+
+    }, (error) => { });
+
+  });
+
+  reset() {
+    window.location.reload();
+  }
+  start() {
+    this.show = false;
+    this.show = true;
+  }
+
 }
---
+++ src/app/shared/services/call-api.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { promise } from 'protractor';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CallApiService {
+
+  constructor(private http:HttpClient) { }
+
+
+  getData(url:string):any{
+
+    return this.http.get(url);
+  }
+
+
+}
